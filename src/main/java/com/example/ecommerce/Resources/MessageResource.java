bef6b07c26c9b0cc6ed29f6ddaede10a84c36d9f
Nguyen Dinh Huy <huynd5@rikkeisoft.com>
2023-11-08 16:07:57 +0700

    fix/update logic time display chat list

--- src/main/java/com/example/ecommerce/Resources/MessageResource.java
+++ src/main/java/com/example/ecommerce/Resources/MessageResource.java
@@ -17,10 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping(path = "/chat")
@@ -32,23 +29,24 @@ public class MessageResource {
     UserService userService;
 
     @GetMapping("/list/{id_user}")
-    ResponseEntity<BaseResponse> getListMessage(@PathVariable("id_user") Integer idUser){
+    ResponseEntity<BaseResponse> getListMessage(@PathVariable("id_user") Integer idUser) {
         User user = userService.findUserById(idUser);
-        if (user == null){
+        if (user == null) {
             return Utils.getResponse(Define.ERROR_CODE, new String[]{"User is not exist"}, null);
         }
         List<MessageModel> listMessage = messageService.getListMessage(user);
         List<ListMessageResponse> listMessageResponse = new ArrayList<>();
-        for (MessageModel messageModel : listMessage){
-            if (idUser == messageModel.getSender().getId()){
+        for (MessageModel messageModel : listMessage) {
+            if (idUser == messageModel.getSender().getId()) {
                 ListMessageResponse messageResponse = new ListMessageResponse(
                         messageModel.getReciever().getId(),
                         messageModel.getReciever().getName(),
                         messageModel.getReciever().getAvatar(),
                         messageModel.getMessage(),
-                        Utils.setUpDateChatList( messageModel.getDate()),
+                        Utils.setUpDateChatList(messageModel.getDate()),
                         true,
-                        0
+                        0,
+                        messageModel.getDate()
                 );
                 listMessageResponse.add(messageResponse);
             } else {
@@ -58,21 +56,39 @@ public class MessageResource {
                         messageModel.getSender().getName(),
                         messageModel.getSender().getAvatar(),
                         messageModel.getMessage(),
-                        Utils.convertDateToString(Utils.DATE_FORMAT_1, messageModel.getDate()),
+                        Utils.setUpDateChatList(messageModel.getDate()),
                         false,
-                        unread
+                        unread,
+                        messageModel.getDate()
                 );
                 listMessageResponse.add(messageResponse);
             }
         }
+
+//        Sap xep lai thu tu tin nhan
+//        Nếu cùng lớn hơn 0 thì so sánh ngày tháng
+//        Nếu 1 thằng = 0 và 1 thăngf khác 0 thì lấy thằng khác 0
+//        Nếu cùng bằng 0 so sánh date
+        listMessageResponse.sort(new Comparator<ListMessageResponse>() {
+            @Override
+            public int compare(ListMessageResponse o1, ListMessageResponse o2) {
+                if (o1.getNumberUnreadMessage() > 0 && o2.getNumberUnreadMessage() > 0) {
+                    return o2.getDate().compareTo(o1.getDate());
+                }
+                if (o2.getNumberUnreadMessage() != o1.getNumberUnreadMessage()) {
+                    return o2.getNumberUnreadMessage() - o1.getNumberUnreadMessage();
+                }
+                return o2.getDate().compareTo(o1.getDate());
+            }
+        });
         return Utils.getResponse(HttpStatus.OK.value(), new String[]{}, listMessageResponse);
     }
 
     @GetMapping("/message")
     ResponseEntity<PagingBaseResponse> gitListMessageUser(@RequestParam(name = "id_user") Integer idUser,
                                                           @RequestParam(name = "id_partner") Integer idPartner,
-                                                          @RequestParam(name = "offset") Integer offset){
-        if (idUser == null || idPartner == null || offset == null){
+                                                          @RequestParam(name = "offset") Integer offset) {
+        if (idUser == null || idPartner == null || offset == null) {
             return ResponseEntity.status(HttpStatus.OK).body(
                     new PagingBaseResponse(HttpStatus.NOT_FOUND.value(), new String[]{"Please send enough infomation"}, null, 0)
             );
@@ -80,7 +96,7 @@ public class MessageResource {
         Page<MessageModel> response = messageService.getListMessage(offset, idUser, idPartner);
         int returnOffset = 0;
         List<MessageModel> listMessageModel = response.getContent();
-        for (MessageModel messageModel: listMessageModel){
+        for (MessageModel messageModel : listMessageModel) {
             messageModel.setFromYou(messageModel.getSender().getId() == idUser);
             messageModel.setFormatDate(Utils.setUpDateDetail(messageModel.getDate()));
         }
@@ -90,7 +106,7 @@ public class MessageResource {
         ArrayList<MessageModel> listMessageResponse = new ArrayList<>(listMessageModel);
         Utils.reverseList(listMessageResponse);
 
-        if (response.getContent().size() % Define.NUMBER_PER_PAGE == 0){
+        if (response.getContent().size() % Define.NUMBER_PER_PAGE == 0) {
             returnOffset = offset + Define.NUMBER_PER_PAGE;
         } else {
             returnOffset = offset + response.getContent().size();
@@ -103,9 +119,9 @@ public class MessageResource {
 
     @GetMapping("/full")
     ResponseEntity<BaseResponse> getFullMessageByUser(@RequestParam(name = "id_sender") Integer idSender,
-                                                          @RequestParam(name = "id_receiver") Integer idReceiver,
-                                                          @RequestParam(name = "offset") Integer offset){
-        if (idSender == null || idReceiver == null || offset == null){
+                                                      @RequestParam(name = "id_receiver") Integer idReceiver,
+                                                      @RequestParam(name = "offset") Integer offset) {
+        if (idSender == null || idReceiver == null || offset == null) {
             return Utils.getResponse(HttpStatus.NOT_FOUND.value(), new String[]{"Please send enough infomation"}, null);
         }
         return Utils.getResponse(HttpStatus.OK.value(), new String[]{}, messageService.getFullMessage(idSender, idReceiver));
